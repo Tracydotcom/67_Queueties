3f9aab97926ad784dbe374c5399b95c2c98f9f33
veliav <lapira.aliahjoyce22176@gmail.com>
2026-01-06 12:26:06 +0800

    may binura

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,25 +1,11 @@
-# INSERTION SORT OF NUMERICAL SORTING
-
 def insertion_sort(arr):
-    for i in range(1, len(arr)): # iterating from second element, as it is considered unsorted. first is always sorted.
+    for i in range(1, len(arr)): 
         j = i
-        while arr[j-1] > arr[j] and j>0: #checks if left element is greater than current
-            arr[j-1], arr[j] = arr[j], arr[j-1] #swaps if left is greater
-            j -= 1 #go further to the left
-            if j == 0: #breaks if it reaches the start of the array
+        while arr[j-1] > arr[j] and j>0: 
+            arr[j-1], arr[j] = arr[j], arr[j-1] 
+            j -= 1 
+            if j == 0: 
                 break
-
-#INSERTION SORT FOR ALPHABETICAL SORTING
-
-# def insertion_sort_alpha(arr):
-#     for i in range(1, len(arr)): # iterating from second element, as it is considered unsorted. first is always sorted.
-#         j = i
-#         while arr[j-1].lower() > arr[j].lower() and j>0: #checks if left element is greater than current (case insensitive)
-#             arr[j-1], arr[j] = arr[j], arr[j-1] #swaps if left is greater
-#             j -= 1 #go further to the left
-#             if j == 0: #breaks if it reaches the start of the array
-#                 break
-
 
 if __name__ == "__main__":
     user = input("Enter numbers separated by spaces: ")
@@ -27,7 +13,3 @@ if __name__ == "__main__":
     insertion_sort(arr)
     print("Sorted array is:", arr)
     
-    # user_alpha = input("Enter words separated by spaces: ")
-    # arr_alpha = user_alpha.split()
-    # insertion_sort_alpha(arr_alpha)
-    # print("Sorted words are:", arr_alpha)
